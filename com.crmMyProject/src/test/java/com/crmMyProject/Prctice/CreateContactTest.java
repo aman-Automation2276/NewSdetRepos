fd6cb1ff3971e05aa78fd7bf143d6b042421b303
aman-Automation2276 <aman2276@gmail.com>
2022-04-02 08:27:44 +0530

    1st time push

---
+++ com.crmMyProject/src/test/java/com/crmMyProject/Prctice/CreateContactTest.java
@@ -0,0 +1,12 @@
+package com.crmMyProject.Prctice;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test
+	public void  createContact()
+	{
+		System.out.println("Contact Created");
+	}
+}
