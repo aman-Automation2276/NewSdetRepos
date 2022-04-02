ec86fc090725d9162f29b6296470e567ce9bff2f
aman-Automation2276 <aman2276@gmail.com>
2022-04-02 08:38:53 +0530

    User 2 commit

---
+++ com.crmMyProject/src/test/java/com/crmMyProject/Prctice/CreateContactByUser2Test.java
@@ -0,0 +1,12 @@
+package com.crmMyProject.Prctice;
+
+import org.testng.annotations.Test;
+
+public class CreateContactByUser2Test {
+
+	@Test
+	public void createContact()
+	{
+		System.out.println("Contact created by user 2");
+	}
+}
