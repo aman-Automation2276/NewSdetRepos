44d482105ffc9582116a05fc4f9c1cbc21f040ae
aman-Automation2276 <aman2276@gmail.com>
2022-04-02 11:37:41 +0530

    push pull command

---
+++ com.crmMyProject/src/test/java/com/crmMyProject/Prctice/PullCommand.java
@@ -0,0 +1,12 @@
+package com.crmMyProject.Prctice;
+
+import org.testng.annotations.Test;
+
+public class PullCommand {
+
+	@Test
+	public void checkPull()
+	{
+		System.out.println("This class should added in other work space");
+	}
+}
